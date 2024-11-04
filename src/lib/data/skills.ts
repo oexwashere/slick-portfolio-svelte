9a0bed5f0c4e48d75829676307102e3e4dca8367
ox <naseemhaidar0717@gmail.com>
2024-11-04 10:17:54 -0600

    feat: add 'Certifications' category and update skill descriptions for JavaScript and TypeScript

--- src/lib/data/skills.ts
+++ src/lib/data/skills.ts
@@ -18,6 +18,7 @@ const categories = [
 	defineSkillCategory({ name: 'Markup & Style', slug: 'markup-style' }),
 	defineSkillCategory({ name: 'Design', slug: 'design' }),
 	defineSkillCategory({ name: 'Soft Skills', slug: 'soft' })
+	defineSkillCategory({ name: 'Certifications', slug: 'certs' })
 ] as const;
 
 const defineSkill = <S extends string>(
@@ -39,7 +40,7 @@ export const items = [
 		slug: 'js',
 		color: 'yellow',
 		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent orci enim, congue sit amet justo eget, consequat sollicitudin libero. Etiam iaculis lectus tempor, hendrerit enim in, luctus arcu. Maecenas id enim et nibh ullamcorper auctor ac eu est. Donec imperdiet, diam quis malesuada faucibus, nibh ex gravida sapien, posuere pharetra nunc libero tristique turpis. Sed egestas laoreet semper. In hac habitasse platea dictumst. Praesent vitae est nec felis maximus facilisis. Duis luctus dui id urna tristique varius. Ut vulputate leo arcu, non bibendum arcu pulvinar eget. Fusce semper elit ut congue lacinia. Suspendisse magna diam, tempus vitae interdum eget, dictum vitae nisl. Praesent quis fringilla tortor. Donec vitae sagittis dui.',
+			'I use JavaScript to make my websites come alive with interactivity and dynamic features. By combining JavaScript with HTML and CSS, I add cool animations, interactive forms, and real-time updates. This way, users can easily explore and engage with my site content. I also use JavaScript frameworks and libraries like React or jQuery to make development faster and easier. This ensures that each site is efficient, scalable, and super user-friendly!',
 		logo: Assets.JavaScript,
 		name: 'Javascript',
 		category: 'pro-lang'
@@ -48,7 +49,7 @@ export const items = [
 		slug: 'ts',
 		color: 'blue',
 		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent orci enim, congue sit amet justo eget, consequat sollicitudin libero. Etiam iaculis lectus tempor, hendrerit enim in, luctus arcu. Maecenas id enim et nibh ullamcorper auctor ac eu est. Donec imperdiet, diam quis malesuada faucibus, nibh ex gravida sapien, posuere pharetra nunc libero tristique turpis. Sed egestas laoreet semper. In hac habitasse platea dictumst. Praesent vitae est nec felis maximus facilisis. Duis luctus dui id urna tristique varius. Ut vulputate leo arcu, non bibendum arcu pulvinar eget. Fusce semper elit ut congue lacinia. Suspendisse magna diam, tempus vitae interdum eget, dictum vitae nisl. Praesent quis fringilla tortor. Donec vitae sagittis dui.',
+			'TypeScript is a programming language created by Microsoft that takes JavaScript to the next level by adding static types. This makes the code easier to read, debug, and scale for bigger projects. With TypeScript, developers can catch errors early on, making the code more reliable and reducing bugs that pop up later. It also supports the latest JavaScript features and is commonly used in frameworks like Angular and in complex applications where maintaining, scaling, and collaborating are important.',
 		logo: Assets.TypeScript,
 		name: 'Typescript',
 		category: 'pro-lang'
