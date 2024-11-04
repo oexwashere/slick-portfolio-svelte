ce83e3c2457aeeff17c33ce56d9187c9d1761c76
ox <naseemhaidar0717@gmail.com>
2024-11-04 11:50:57 -0600

    feat: add Photoshop, Premiere, and NextJs skills; update asset imports and logos

--- src/lib/data/assets.ts
+++ src/lib/data/assets.ts
@@ -1,6 +1,6 @@
-import type { Asset } from '$lib/types';
-import { theme } from '$lib/stores/theme';
 import { base } from '$app/paths';
+import { theme } from '$lib/stores/theme';
+import type { Asset } from '$lib/types';
 
 const gh = (file: string) => `${base}/logos/${file}`;
 
@@ -70,7 +70,8 @@ const Assets = {
 	Jest: a('jest.png'),
 	Unocss: a('unocss.svg'),
 	Ruvy: a('ruvy.svg'),
-	Postcss: a('postcss.svg')
+	Postcss: a('postcss.svg'),
+	NextJs: a('nextjs.svg'),
 };
 
 export default Assets;
--- src/lib/data/skills.ts
+++ src/lib/data/skills.ts
@@ -99,13 +99,31 @@ export const items = [
 		category: 'library'
 	}),
 	defineSkill({
-		slug: 'tailwind',
-		color: 'cyan',
+		slug: 'photoshop',
+		color: 'blue',
 		description:
 			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent orci enim, congue sit amet justo eget, consequat sollicitudin libero. Etiam iaculis lectus tempor, hendrerit enim in, luctus arcu. Maecenas id enim et nibh ullamcorper auctor ac eu est. Donec imperdiet, diam quis malesuada faucibus, nibh ex gravida sapien, posuere pharetra nunc libero tristique turpis. Sed egestas laoreet semper. In hac habitasse platea dictumst. Praesent vitae est nec felis maximus facilisis. Duis luctus dui id urna tristique varius. Ut vulputate leo arcu, non bibendum arcu pulvinar eget. Fusce semper elit ut congue lacinia. Suspendisse magna diam, tempus vitae interdum eget, dictum vitae nisl. Praesent quis fringilla tortor. Donec vitae sagittis dui.',
-		logo: Assets.ReactJs,
-		name: 'React Js',
-		category: 'library'
+		logo: Assets.Photoshop,
+		name: 'Photoshop',
+		category: 'design'
+	}),
+	defineSkill({
+		slug: 'premiere',
+		color: 'pink',
+		description:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent orci enim, congue sit amet justo eget, consequat sollicitudin libero. Etiam iaculis lectus tempor, hendrerit enim in, luctus arcu. Maecenas id enim et nibh ullamcorper auctor ac eu est. Donec imperdiet, diam quis malesuada faucibus, nibh ex gravida sapien, posuere pharetra nunc libero tristique turpis. Sed egestas laoreet semper. In hac habitasse platea dictumst. Praesent vitae est nec felis maximus facilisis. Duis luctus dui id urna tristique varius. Ut vulputate leo arcu, non bibendum arcu pulvinar eget. Fusce semper elit ut congue lacinia. Suspendisse magna diam, tempus vitae interdum eget, dictum vitae nisl. Praesent quis fringilla tortor. Donec vitae sagittis dui.',
+		logo: Assets.Premiere,
+		name: 'Premiere',
+		category: 'design'
+	}),
+	defineSkill({
+		slug: 'nextjs',
+		color: 'black',
+		description:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent orci enim, congue sit amet justo eget, consequat sollicitudin libero. Etiam iaculis lectus tempor, hendrerit enim in, luctus arcu. Maecenas id enim et nibh ullamcorper auctor ac eu est. Donec imperdiet, diam quis malesuada faucibus, nibh ex gravida sapien, posuere pharetra nunc libero tristique turpis. Sed egestas laoreet semper. In hac habitasse platea dictumst. Praesent vitae est nec felis maximus facilisis. Duis luctus dui id urna tristique varius. Ut vulputate leo arcu, non bibendum arcu pulvinar eget. Fusce semper elit ut congue lacinia. Suspendisse magna diam, tempus vitae interdum eget, dictum vitae nisl. Praesent quis fringilla tortor. Donec vitae sagittis dui.',
+		logo: Assets.NextJs,
+		name: 'NextJs',
+		category: 'framework'
 	}),
 ] as const;
 
