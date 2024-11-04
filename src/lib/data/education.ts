323607013b91b4ee8411f3ec53a4636e26ce4fb7
ox <naseemhaidar0717@gmail.com>
2024-11-04 10:07:55 -0600

    chore: update package.json to use caret (^) for versioning on adapter-static and svelte

--- src/lib/data/education.ts
+++ src/lib/data/education.ts
@@ -1,31 +1,19 @@
-import Assets from './assets';
 import type { Education } from '../types';
+import Assets from './assets';
 
 export const items: Array<Education> = [
 	{
-		degree: 'Bachelor degree of Computer Science',
+		degree: 'Bachelor degree of Networking and Security (CCNA Pathway)',
 		description: '',
-		location: 'Tunisia',
+		location: 'Utah, USA',
 		logo: Assets.Unknown,
 		name: '',
-		organization: 'ISTIC',
-		period: { from: new Date(2020, 0, 1), to: new Date(2022, 5, 1) },
+		organization: 'WGU',
+		period: { from: new Date(2023, 1, 1), to: new Date(2026, 12, 26) },
 		shortDescription: '',
 		slug: 'dummy-education-item',
-		subjects: ['C', 'Algorithm', 'Algebra', 'Python', 'C++', 'Java', 'English']
+		subjects: ['C', 'Algorithm', 'Algebra', 'Python', 'C++', 'Java', 'English', 'Networking', 'Security']
 	},
-	{
-		degree: 'PhD of Computer Science',
-		description: '',
-		location: 'USA',
-		logo: Assets.Unknown,
-		name: '',
-		organization: 'MIT',
-		period: { from: new Date(2023, 0, 1) },
-		shortDescription: '',
-		slug: 'dummy-education-item-2',
-		subjects: ['Assembly', 'Rust', 'Computer Architecture', 'Algorithms and Data structures']
-	}
 ];
 
 export const title = 'Education';
--- src/lib/data/home.ts
+++ src/lib/data/home.ts
@@ -3,39 +3,35 @@ import { getSkills } from './skills';
 
 export const title = 'Home';
 
-export const name = 'Name';
+export const name = 'Naseem';
 
-export const lastName = 'LastName';
+export const lastName = 'Haidar';
 
 export const description =
-	'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, rerum. Debitis accusantium deleniti enim iste dignissimos? Similique, exercitationem! Odit vero, numquam quae ratione maxime sunt reiciendis laudantium quaerat iure ipsum!';
+	'Welcome to my portfolio! I’m Naseem Haidar, a Data Analyst and Safety Specialist with a passion for transforming complex data into actionable insights that drive efficiency and safety. With a background in IT, emergency medical services, and data analysis, I’m skilled at solving real-world challenges through data-driven decision-making and cross-functional collaboration. Explore my projects to see how I combine analytical expertise and innovative problem-solving to deliver impactful results across various fields.';
 
 export const links: Array<{ platform: Platform; link: string }> = [
-	{ platform: Platform.GitHub, link: 'https://github.com/' },
+	{ platform: Platform.GitHub, link: 'https://github.com/oexwashere' },
 	{
 		platform: Platform.Linkedin,
-		link: 'https://www.linkedin.com/'
+		link: 'https://www.linkedin.com/in/naseemhaidar/'
 	},
 	{
 		platform: Platform.Twitter,
-		link: 'https://twitter.com/'
+		link: 'https://twitter.com/oexwashere'
 	},
 	{
 		platform: Platform.StackOverflow,
-		link: 'https://stackoverflow.com/'
+		link: 'https://stackoverflow.com/users/28129551/oex'
 	},
 	{
 		platform: Platform.Email,
-		link: 'riadh_adrani@hotmail.fr'
+		link: 'contact@nhaidar.dev'
 	},
 	{
 		platform: Platform.Youtube,
-		link: 'https://www.youtube.com'
+		link: 'https://www.youtube.com/oexwashere'
 	},
-	{
-		platform: Platform.Facebook,
-		link: 'https://www.facebook.com'
-	}
 ];
 
 export const skills = getSkills('js', 'css', 'html', 'reactjs', 'sass', 'svelte', 'ts');
